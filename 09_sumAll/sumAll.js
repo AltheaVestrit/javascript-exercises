79bad94741dab833c42d86451111efb64fa297f6
Althea Vestrit <137403706+AltheaVestrit@users.noreply.github.com>
2025-06-18 21:59:36 +0200

    Complete exercises 06 - 11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    if (Number.isInteger(start) && Number.isInteger(end) && start >= 0 && end >= 0) {
+        let finalSum = 0;
+        for (i = Math.min(start, end); i <= Math.max(start, end); i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
